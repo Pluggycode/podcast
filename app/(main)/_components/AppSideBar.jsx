803c4e682526d76d7c47bdebe6c25119744eabbc
shashidhar k <160392735+Pluggycode@users.noreply.github.com>
2025-02-24 22:07:09 +0530

    Update AppSideBar.jsx

--- app/(main)/_components/AppSideBar.jsx
+++ app/(main)/_components/AppSideBar.jsx
@@ -60,19 +60,12 @@ function AppSideBar() {
                     <SidebarGroup>
                         <SidebarGroupContent>
                             <div className="mx-6 mt-8">
-                            <Link href={'/create-new-podcast'}><Button className="w-full mt-3 "> Create New Podcast</Button></Link>
+                            <Link href={'/create-new-podcast'}><Button className="w-full mt-3 "> Create New Podcast</Button></Link
+                            </div>
+                            <SidebarMenu>
                                 <Link href={'/dashboard'}><Button className="w-full mt-3"> Home </Button></Link>
                                 <Link href={'/explore'}><Button className="w-full mt-3"> explore </Button></Link>
                                 <Link href={'/billing'}><Button className="w-full mt-3"> Billing</Button></Link>
-                            </div>
-                            <SidebarMenu>
-                                {menuItems.map((menu,index) => (
-                                    <SidebarMenuItem className="mt-3" key={index}>
-                                        <SidebarMenuButton className="p-5 bg-nav-focus border-r-4 border-orange-1" isActive={path==menu.url}>
-                                         
-                                        </SidebarMenuButton>
-                                    </SidebarMenuItem>
-                                ))}
                             </SidebarMenu>
                         </SidebarGroupContent>
                     </SidebarGroup>

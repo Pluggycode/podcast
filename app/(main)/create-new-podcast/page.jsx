a4a37b539be0fea03cec3ee391d22c04dfe38b1a
Pluggycode <mrmagnums33@gmail.com>
2025-02-25 14:29:37 +0530

    Resolved merge conflicts in route.jsx

--- app/(main)/create-new-podcast/page.jsx
+++ app/(main)/create-new-podcast/page.jsx
@@ -1,6 +1,8 @@
 "use client"
-import React, { useState } from 'react'
-import Topic from './_components/Topic'
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Topic from './_components/Topic';
 import PodcastVideoStyle from './_components/PodcastVideoStyle';
 import Voice from './_components/Voice';
 import Captions from './_components/Captions';
@@ -12,62 +14,72 @@ import { api } from '../../../convex/_generated/api';
 import { Loader2Icon, RefreshCcw, WandSparkles } from 'lucide-react';
 import axios from 'axios';
 import Link from 'next/link';
+
 function CreateNewPodcast() {
-    const [formData, setFormData] = useState();
+    const [formData, setFormData] = useState({});
     const { user } = userAuthContext();
     const CreateInitialPodcastRecord = useMutation(api.podcastData.createPodcastData);
-    const [loading,setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [aloading, setaLoading] = useState(false);
+
     const onHandleInputChange = (filedName, fieldValue) => {
         setFormData(prev => ({
             ...prev,
             [filedName]: fieldValue
-        }))
-
+        }));
         console.log(formData);
-
-    }
+    };
 
     const generatePodcast = async () => {
-
-        if(user?.credits<=0)
-        {
-            toast('Please add more credits')
-            return ;
-        }
-        if (!formData?.topic || !formData?.script || !formData?.captions || !formData?.videoStyle || !formData?.voice) {
-            console.log("Error", "Enter all the fields")
+        if (user?.credits <= 0) {
+            toast.error('Please add more credits');
             return;
         }
 
-        //Save video data first 
+        if(!formData?.title || !formData?.captions || !formData?.videoStyle || !formData?.voice) {
+            if(!formData?.topic || !formData?.script) {
+                toast.info("select topic and script");
+                return;
+            }
+            else{
+                toast.warn("Enter all the fields");
+                toast.info
+                return;
+            }
+        }
+        else {
+            toast.success("Podcast generated successfully.., check your dashboard");
+        }
+
+        // Save video data first 
         setLoading(true);
 
         const resp = await CreateInitialPodcastRecord({
             title: formData.title,
             topic: formData.topic,
-            script:formData.script,
+            script: formData.script,
             videoStyle: formData.videoStyle,
             caption: formData.captions,
             voice: formData.voice,
             uid: user?._id,
             createdBy: user?.email,
-            credits:user?.credits,
-            userImageUrl:user.userImageUrl,
-            userName:user?.name
-        })
+            credits: user?.credits,
+            userImageUrl: user.userImageUrl,
+            userName: user?.name
+        });
         console.log(resp);
 
-       const result = await axios.post('/api/generate-podcast-data', {
+        const result = await axios.post('/api/generate-podcast-data', {
             ...formData,
-            recordId:resp,
-            
-         })
+            recordId: resp,
+        });
         console.log(result);
         setLoading(false);
-    }
+    };
 
     return (
         <div className=''>
+            <ToastContainer />
             <h2 className='text-2xl'>Create new podcast</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
                 <div className="col-span-2 p-10 border rounded-xl h-[72vh] overflow-auto mt-16">
@@ -75,7 +87,6 @@ function CreateNewPodcast() {
                     <Topic onHandleInputChange={onHandleInputChange} />
 
                     {/* video image style */}
-
                     <PodcastVideoStyle onHandleInputChange={onHandleInputChange} />
 
                     {/* voice */}
@@ -84,13 +95,16 @@ function CreateNewPodcast() {
                     {/* captions */}
                     <Captions onHandleInputChange={onHandleInputChange} />
 
-                  <Link href={'/dashboard'} ><Button disabled={loading} className="w-full mt-5 " onClick={generatePodcast}>{loading?<RefreshCcw  className='animate-spin'/>:<WandSparkles />}Generate Podcast</Button> </Link>
+                    <Button disabled={loading} className="w-full mt-5" onClick={generatePodcast}>
+                        {loading ? <RefreshCcw className='animate-spin' />:<WandSparkles />} Generate Podcast
+                    </Button>
+
+                    <Link href={'/dashboard'}><Button className="mt-5 w-full bg-slate-800 border text-gray-100 hover:border-gray-100 hover:text-slate-800 hover:bg-gray-200" disabled={loading}>Dashboard</Button></Link>
                 </div>
                 <Preview formData={formData} />
             </div>
-
         </div>
-    )
+    );
 }
 
-export default CreateNewPodcast
+export default CreateNewPodcast;

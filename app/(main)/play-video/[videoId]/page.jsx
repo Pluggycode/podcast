9ef2cb727277f3f1758e563a9c876b96efbab8df
shashidhar k <160392735+Pluggycode@users.noreply.github.com>
2025-02-25 20:37:05 +0530

    Update page.jsx

--- app/(main)/play-video/[videoId]/page.jsx
+++ app/(main)/play-video/[videoId]/page.jsx
@@ -25,7 +25,7 @@ function Platvideo() {
 
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 gap-10 md:p-6 lg:p-8'>
-            <div className="w-full">
+            <div className="w-full bg-white rounded-md">
                 {/* RemotionPlayer */}
                 <RemotionPlayer videoData={videoData} />
             </div>

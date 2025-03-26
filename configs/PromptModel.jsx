3fb1047be1b1430a20af5ed76be83270b224c880
shashidhar k <160392735+Pluggycode@users.noreply.github.com>
2025-03-26 11:56:13 +0530

    Update PromptModel.jsx

--- configs/PromptModel.jsx
+++ configs/PromptModel.jsx
@@ -8,7 +8,7 @@ const {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash-lite",
+    model: "gemini-2.0-flash",
   });
   
   const generationConfig = {

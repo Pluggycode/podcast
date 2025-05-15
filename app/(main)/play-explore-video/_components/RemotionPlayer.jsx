72a24a8206917c55ebb740d012d2717ff0a0a1f4
shashidhar k <160392735+Pluggycode@users.noreply.github.com>
2025-05-15 23:24:30 +0530

    Update RemotionPlayer.jsx

--- app/(main)/play-explore-video/_components/RemotionPlayer.jsx
+++ app/(main)/play-explore-video/_components/RemotionPlayer.jsx
@@ -1,1 +1,31 @@
+'use client'
+import React, { useState } from 'react'
+import { Player } from '@remotion/player'
+import RemotionComposition from '../../../../app/_components/RemotionComposition'
+
+function RemotionPlayer({ videoData }) {
+    const [durationInFrame, setDurationInFrame] = useState(100);
+    return (
+        <div className="w-full max-w-screen-md mx-auto">
+            <Player
+                component={RemotionComposition}
+                durationInFrames={Number(durationInFrame.toFixed(0)) + 100}
+                compositionWidth={800}
+                compositionHeight={1200}
+                fps={30}
+                controls
+                style={{
+                    width: '40vh',
+                    height: '60vh',
+                }}
+                inputProps={{
+                    videoData: videoData,
+                    setDurtionInFrame: (frameValue) => setDurationInFrame(frameValue)
+                }}
+            />
+        </div>
+    )
+}
+
+export default RemotionPlayer
 
